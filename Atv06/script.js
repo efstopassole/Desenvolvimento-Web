672c23d2fc93defde715a73e01e20a79e817c6c2
Enzostp <efstopassole@minha.fag.edu.br>
2026-04-14 19:15:25 -0300

    ATV7

--- Atv06/script.js
+++ Atv06/script.js
@@ -1,4 +1,40 @@
-console.log(document)
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
 
-const titulo = document.querySelector('h1')
-console.log(titulo)
+function addTask() {
+    const taskValue = taskInput.value.trim();
+
+    if (taskValue === "") {
+        alert("Por favor, digite uma tarefa!");
+        return;
+    }
+
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+        <span>
+            <input type="checkbox" class="complete-check">
+            <span class="task-text">${taskValue}</span>
+        </span>
+    `;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+    taskInput.focus();
+}
+
+addTaskBtn.addEventListener('click', addTask);
+
+taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') addTask();
+});
+
+taskList.addEventListener('click', (event) => {
+
+    if (event.target.tagName === 'LI' || event.target.className === 'task-text') {
+        const itemToRemove = event.target.closest('li');
+        itemToRemove.remove();
+    }
+});
